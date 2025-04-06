4a6fd46c37c23ccd8e3ec811829c6a69a5f9d9fc
mkizka <g.mkizka@gmail.com>
2025-04-06 14:12:07 +0900

    init

---
+++ build.js
@@ -0,0 +1,11 @@
+import { build } from 'esbuild';
+
+await build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  format: 'esm',
+  outdir: 'dist',
+  minify: true,
+  sourcemap: true,
+})
